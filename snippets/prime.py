fc9d61dc663c09b0cc87b902400f43ad7d82310f
simaki <shota.imaki@icloud.com>
2020-05-11 10:58:36 +0900

    add is_prime

--- snippets/prime.py
+++ snippets/prime.py
@@ -1,5 +1,33 @@
 
 
+def is_prime(n):
+    """
+    Return if a number is a prime number.
+
+    Complexity
+    ----------
+    O(n^{1/2})
+
+    Examples
+    --------
+    >>> [is_prime(i) for i in range(1, 10)]
+    [False, True, True, False, True, False, True, False, False]
+    >>> is_prime(57)
+    False
+    >>> is_prime(10 ** 9 + 7)
+    True
+    """
+    if n == 1:
+        return False
+
+    m = min(n - 1, int(pow(n, 1/2)) + 1)
+
+    for i in range(2, m + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
 def prime(n):
     """
     Return prime numbers <= n.
