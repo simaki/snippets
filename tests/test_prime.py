c3ad7dd2cd44345588c834c2d55bc80b6f419bfc
simaki <shota.imaki@icloud.com>
2020-05-11 21:46:56 +0900

    add test_prime

---
+++ tests/test_prime.py
@@ -0,0 +1,49 @@
+from os.path import dirname
+from pathlib import Path
+
+import pytest
+
+from snippets import prime
+
+
+def solve(i):
+    """
+    Examples
+    --------
+    >>> solve(10)
+    4
+    >>> solve(100)
+    25
+    """
+    return len(prime(i))
+
+
+def f_in_out():
+    cases = Path(dirname(__file__)) / 'library-checker-problems/math/counting_primes/'
+    f = []
+    for f_in, f_out in zip(sorted(cases.glob('in/small_*.in')), sorted(cases.glob('out/small_*.out'))):
+        f.append([f_in, f_out])
+    return f
+
+
+f = f_in_out()
+
+
+class TestSolve:
+
+    solve = solve
+
+    @classmethod
+    def _test_for_case(cls, f_in, f_out, f_a=None):
+        with open(f_in) as f:
+            i = int(f.read())
+        with open(f_out) as f:
+            o_expected = int(f.read())
+
+        assert cls.solve(i) == o_expected
+
+    @pytest.mark.parametrize('f', f)
+    def test(self, f):
+        f_in, f_out = f
+        self._test_for_case(f_in, f_out)
+
