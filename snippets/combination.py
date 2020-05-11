e89c5582f634bd47a2adce71d281d317f6145107
simaki <shota.imaki@icloud.com>
2020-05-11 10:10:56 +0900

    add combination

--- snippets/combination.py
+++ snippets/combination.py
@@ -61,6 +61,71 @@ def combination(n, k, mod):
     return (factorial(n, mod) * dinv) % mod
 
 
+class Combination:
+    """
+    Return Combination (n, k) modulo mod.
+
+    Complexity
+    ----------
+    O(n) for Initialization, O(1) for call.
+
+    Attributes
+    ----------
+    - f : list of int, length n + 1
+        f = [i! modulo mod for i = 0, ..., n]
+    - fi : list of int, length n + 1
+        fi = [i!^-1 modulo mod for i = 0, ..., n]
+
+    Examples
+    --------
+    >>> MOD = 10 ** 9 + 7
+    >>> c = Combination(100, MOD)
+    >>> c.f[:10]
+    [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
+    >>> c(50)
+    538992043
+    """
+    def __init__(self, n, mod):
+        self.n = n
+        self.mod = mod
+
+        self.f = self._get_f()
+        self.fi = self._get_fi()
+
+    def __call__(self, k):
+        if not 0 <= k <= self.n:
+            return 0
+        else:
+            num = self.f[self.n]
+            deninv = (self.fi[k] * self.fi[self.n - k]) % self.mod
+            return (num * deninv) % self.mod
+
+    def _get_f(self):
+        f = 1
+        lf = [1]
+
+        for i in range(1, self.n + 1):
+            f = (f * i) % self.mod
+            lf.append(f)
+
+        return lf
+
+    def _get_fi(self):
+        ii = 1
+        lii = [0, 1]  # [i^-1 modulo mod for i in 0, ..., n]
+        fi = 1
+        lfi = [1, 1]
+
+        for i in range(2, self.n + 1):
+            ii = (- lii[self.mod % i] * (self.mod // i)) % self.mod
+            lii.append(ii)
+
+            fi = (fi * ii) % self.mod
+            lfi.append(fi)
+
+        return lfi
+
+
 if __name__ == '__main__':
     from doctest import testmod
 
